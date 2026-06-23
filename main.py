8fab001d9acb141f1570afc3a3776bbffad091da
erdosblanka28 <erdosblanka28@gmail.com>
2026-06-23 22:18:31 +0200

    data loading and saving

--- main.py
+++ main.py
@@ -24,7 +24,26 @@ with open(path, "r") as file: #with automatically closes file
         song_folder_name = os.path.basename(song_folder) #gets the name of genre folder
         new_path = os.path.join(path_search,"genres",song_folder_name,song_file_name)
 
-        song_raw.append([new_path,genres])
+        song_raw.append((new_path,genres))
 
 ##print({len(song_raw)}) testing
 ##print(song_raw[0]) testing
+
+mfcc = []
+label = []
+
+for new_path, genres in song_raw:
+    
+    amplitude, sampling_rate = librosa.load(new_path)
+    mfcc_1 = librosa.feature.mfcc(y=amplitude, sr=sampling_rate, n_mfcc=20) #cuts the songs into ~25ms pieces, saving the 20 first coefficient
+    mfcc_mean = np.mean(mfcc_1, axis=1) #taking the mean for each row (20 seperate mfcc value averaged over the whole song)
+   #saving to list
+    mfcc.append(mfcc_mean)
+    label.append((genres))
+
+#converting,compressing & saving the data
+x = np.array(mfcc)
+y = np.array(label)
+np.savez_compressed("music_features.npz", features=x,labels=y)
+
+print("music_features.npz is successfully saved to current folder.")
